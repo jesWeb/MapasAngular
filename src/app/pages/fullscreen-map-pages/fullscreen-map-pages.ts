0c9db5ef6d6bdee6f02ca081817d54073fee9120
jesus <jesus@2806-104e-0013-d90d-0000-0000-0000-0002.ipv6.infinitum.net.mx>
2025-11-14 17:56:27 -0600

    ultimos avances

--- src/app/pages/fullscreen-map-pages/fullscreen-map-pages.ts
+++ src/app/pages/fullscreen-map-pages/fullscreen-map-pages.ts
@@ -1,43 +1,85 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
+
 import mapboxgl from 'mapbox-gl';
-import { EnvironmentInjector } from '@angular/core';
-mapboxgl.accessToken = 'environment.mapboxKey';
+import { environment } from '../../../environments/environment';
+import { DecimalPipe } from '@angular/common';
+
+mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-fullscreen-map-pages',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './fullscreen-map-pages.html',
   styles: `
-  div{
-    width:100vw;
-    height:calc( 100vh - 64px);
-  }
-  `,
+    div {
+      width: 100vw;
+      height: calc( 100vh - 64px);
+    }
+
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }  `
 })
 export class FullscreenMapPages implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null)
+
+  //zoom
+  Zoom = signal(14)
+
+  //efect
+  ZoomEffect = effect(() => {
+
+    if (!this.map()) {
+      return;
+    }
+
+    this.map()?.setZoom(this.Zoom())
+
+
+  })
 
 
   //afterViewInit despues de cargar
   async ngAfterViewInit() {
 
-    if (!this.divElement) return;
+    if (!this.divElement()?.nativeElement) return;
 
-    await new Promise((resolve) =>setTimeout(resolve,80));
+    await new Promise((resolve) => setTimeout(resolve, 80));
 
     const element = this.divElement()!.nativeElement;
     console.log(element);
 
 
     const map = new mapboxgl.Map({
-      container: 'map', // container ID
+      container: element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.Zoom(), // starting zoom
     });
 
-
+      this.mapListeners(map)
 
   }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.Zoom.set(newZoom)
+    })
+
+    this.map.set(map)
+
+  }
+
 
 }
