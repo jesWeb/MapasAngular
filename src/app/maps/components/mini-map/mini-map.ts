0b0264d40aacc9a6995b43f10751b0c8b09d20cf
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-11-17 17:27:54 -0600

    mini mapas

---
+++ src/app/maps/components/mini-map/mini-map.ts
@@ -0,0 +1,57 @@
+import { AfterViewInit, Component, ElementRef, input, viewChild } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = environment.mapboxKey
+
+@Component({
+  selector: 'app-mini-map',
+  imports: [],
+  templateUrl: './mini-map.html',
+  styles: `
+  div{
+    width:100%;
+    height:260px;
+  }`
+})
+
+/**
+ * width 100%
+ * heigth 260
+ */
+export class MiniMap implements AfterViewInit {
+
+  divElement = viewChild<ElementRef>('map');
+  lnlagt = input.required<{ lng: number, lat: number }>()
+  zoom = input<number>(16)
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lnlagt(), // starting position [lng, lat]
+      zoom: this.zoom(), // starting zoom
+      interactive:false,
+      pitch:30
+    });
+
+
+    new mapboxgl.Marker().setLngLat(this.lnlagt()).addTo(map)
+
+
+
+
+
+
+  }
+
+
+
+
+}
